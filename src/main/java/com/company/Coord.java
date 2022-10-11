75fd68a342d9b427a59ee409714ba7e5174be3dc
krzysztof.grabowski <krzysztof.grabowski@cronn.de>
2022-10-11 22:38:24 +0200

    Coord entity added

---
+++ src/main/java/com/company/Coord.java
@@ -0,0 +1,50 @@
+
+package com.company;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import javax.annotation.processing.Generated;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "lat",
+        "lon"
+})
+@Generated("jsonschema2pojo")
+public class Coord {
+
+    @JsonProperty("lat")
+    private Double lat;
+    @JsonProperty("lon")
+    private Double lon;
+
+    @JsonProperty("lat")
+    public Double getLat() {
+        return lat;
+    }
+
+    @JsonProperty("lat")
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    @JsonProperty("lon")
+    public Double getLon() {
+        return lon;
+    }
+
+    @JsonProperty("lon")
+    public void setLon(Double lon) {
+        this.lon = lon;
+    }
+
+    @Override
+    public String toString() {
+        return "Coord{" +
+                "lat=" + lat +
+                ", lon=" + lon +
+                '}';
+    }
+}
