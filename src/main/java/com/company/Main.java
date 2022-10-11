91ac2990f85994edfd56a0cd4112f717a348f9e8
krzysztof.grabowski <krzysztof.grabowski@cronn.de>
2022-10-11 22:40:42 +0200

    City object reading from response JSON example

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,10 +1,18 @@
 package com.company;
 
+import com.alibaba.fastjson.JSON;
+import org.json.JSONObject;
+
 public class Main {
 
     public static void main(String[] args) {
         WeatherService weatherService = new WeatherService();
 
-        System.out.println(weatherService.fetchForecast());
+        String jsonString = weatherService.fetchForecast();
+        JSONObject obj = new JSONObject(jsonString);
+        String pageName = obj.getJSONObject("city").toString();
+        City city = JSON.parseObject(pageName, City.class);
+
+        System.out.println(city);
     }
 }
